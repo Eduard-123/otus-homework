9c33f6eced99be87b177257162cf31ddd9645bde
Traumerei <eduard.borisov94@gmail.com>
2021-01-05 18:17:02 +0300

    sequence of numbers

--- hw15/src/main/java/SynchronizedPrint.java
+++ hw15/src/main/java/SynchronizedPrint.java
@@ -1,37 +1,39 @@
-import static java.lang.String.format;
+import java.util.stream.IntStream;
 
 public class SynchronizedPrint implements Runnable {
 
-    private final Object monitor = new Object();
+    private String lastThread = "thread2";
 
     @Override
     public void run() {
-        try {
-            int i = 1;
-            while (true) {
-                printSequence(i);
+        while (true) {
+            iterate(IntStream.range(1, 11));
+            try {
                 Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    private void printSequence(int i) throws InterruptedException {
-        for (int j = 1; j < 10; j++) {
-            synchronized (monitor) {
-                monitor.notify();
-                System.out.print(format("%s(%s)\n ", i++, Thread.currentThread().getName()));
-                monitor.wait();
-            }
-        }
-        for (int k = 1; k < 10; k++) {
-            synchronized (monitor) {
-                monitor.notify();
-                System.out.print(format("%s(%s)\n ", i--, Thread.currentThread().getName()));
-                monitor.wait();
-            }
+            iterate(IntStream.range(1, 11).map(i -> 11 - i));
         }
     }
+
+    private void iterate(IntStream range) {
+        range.forEach(value -> {
+                    synchronized (this) {
+                        try {
+                            while (Thread.currentThread().getName().equals(lastThread)) {
+                                this.wait();
+                            }
+                            System.out.println(String.format("[ %s ] - %d",Thread.currentThread().getName(), value));
+                            lastThread = Thread.currentThread().getName();
+                            Thread.sleep(500);
+                            notifyAll();
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+    }
+
 }
